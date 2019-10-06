51152dd5af2b4fcebcceca30b1791d2c4ba19da1
Luuc Verburgh <luuc.verburgh@gmail.com>
2019-10-06 21:47:04 +0200

    animates svg from previous to current values

--- src/RadarCircle.js
+++ src/RadarCircle.js
@@ -1,6 +1,9 @@
 // @flow
-import React from 'react';
+import React, {useEffect} from 'react';
 import _ from 'lodash';
+import {styler, tween, easing} from 'popmotion';
+import posed from 'react-pose';
+import {interpolate} from 'flubber';
 import {radialLine, curveCardinalClosed} from 'd3-shape';
 import type {TickScale, RadarPoint} from './types';
 
@@ -26,73 +29,99 @@ const defaultCircleStyle = {
   inactivePointOpacity: 0.7,
 };
 
-export default function RadarCircle(props: RadarCircleProps) {
-  const {
-    currentPoints,
-    previousPoints,
-    scales,
-    offsetAngles,
-    isSelected,
-    color,
-    selectedVariableKey,
-    style,
-  } = props;
-  const {
-    selectedFillOpacity,
-    inactiveFillOpacity,
-    selectedStrokeOpacity,
-    inactiveStrokeOpacity,
-    pointRadius,
-    selectedPointFill,
-    selectedPointOpacity,
-    inactivePointOpacity,
-  } = {...defaultCircleStyle, ...style};
+const morphTransition = ({ from, to }) => tween({
+  from: 0,
+  to: 1,
+  duration: 1000,
+  ease: easing.easeInOut
+}).pipe(interpolate(from, to));
 
-  const lineFunction = radialLine()
-    .radius((point: RadarPoint) => scales[point.variableKey](point.value))
-    .angle((point: RadarPoint) => _.round(offsetAngles[point.variableKey], 6))
+const Shape = posed.path(
+  {
+    "current": {
+      "d": ({ currentPath }) => currentPath,
+      transition: morphTransition,
+    },
+    "previous": {
+      "d": ({ previousPath }) => previousPath,
+      transition: morphTransition,
+    },
+  }
+);
+
+class RadarCircle extends React.Component<RadarCircleProps> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        previousPath: this.lineFunction(this.props.previousPoints),
+        currentPath: this.lineFunction(this.props.currentPoints),
+        keys: ['current', 'previous'],
+        index: 0,
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps != this.props) {
+      this.setState({
+        previousPath: this.lineFunction(this.props.previousPoints),
+        currentPath: this.lineFunction(this.props.currentPoints),
+      })
+    }
+  }
+
+  componentDidMount() {
+    console.log('mounted radar circle');
+    setTimeout(() => {
+      console.log('should have animated the fucking thing');
+      this.setState({
+        index: 1,
+      })
+    }, 5000);
+  }
+
+  lineFunction = radialLine()
+    .radius((point: RadarPoint) => this.props.scales[point.variableKey](point.value))
+    .angle((point: RadarPoint) => _.round(this.props.offsetAngles[point.variableKey], 6))
     .curve(curveCardinalClosed);
 
-  console.log('currentPoints', currentPoints);
-  const previousPathData = lineFunction(previousPoints);
-  const currentPathData = lineFunction(currentPoints);
-  return (
-    <g>
-      <path
-        d={previousPathData}
-        fill={color}
-        fillOpacity={isSelected ? selectedFillOpacity : inactiveFillOpacity}
-        stroke={color}
-        strokeOpacity={
-          isSelected ? selectedStrokeOpacity : inactiveStrokeOpacity
-        }
-      />
-      <path
-        d={currentPathData}
-        fill={color}
-        fillOpacity={isSelected ? selectedFillOpacity : inactiveFillOpacity}
-        stroke={color}
-        strokeOpacity={
-          isSelected ? selectedStrokeOpacity : inactiveStrokeOpacity
-        }
-      />
-      {/* {points.map(point => {
-        return (
-          <circle
-            key={point.key}
-            r={pointRadius}
-            fill={
-              point.variableKey === selectedVariableKey
-                ? selectedPointFill
-                : color
-            }
-            stroke={color}
-            cx={point.x}
-            cy={point.y}
-            opacity={isSelected ? selectedPointOpacity : inactivePointOpacity}
-          />
-        );
-      })} */}
-    </g>
-  );
+  render() {
+    const {
+      isSelected,
+      color,
+      style,
+    } = this.props;
+
+    const {
+      index,
+      keys,
+      previousPath,
+      currentPath,
+    } = this.state;
+
+    const {
+      selectedFillOpacity,
+      inactiveFillOpacity,
+      selectedStrokeOpacity,
+      inactiveStrokeOpacity,
+    } = {...defaultCircleStyle, ...style};
+
+    return (
+      <g>
+        <Shape
+          pose={keys[index]}
+          previousPath={previousPath}
+          currentPath={currentPath}
+          fill={color}
+          fillOpacity={isSelected ? selectedFillOpacity : inactiveFillOpacity}
+          stroke={color}
+          strokeOpacity={
+            isSelected ? selectedStrokeOpacity : inactiveStrokeOpacity
+          }
+        />
+      </g>
+    );
+  };
 }
+
+export default RadarCircle;
